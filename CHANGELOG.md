31c7d945f9a091d6c794ada7e8c923f4da43f85a
e3s1 <126372694+e3s1@users.noreply.github.com>
2024-10-21 21:33:12 -0400

    1.0.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,5 @@
 # Changelog
+## 1.0.1
+- Cosmetics are now properly unhidden when switching players or when revived
 ## 1.0.0
 - Release
--- Unobscured/Patches/HideCosmeticsPatch.cs
+++ Unobscured/Patches/HideCosmeticsPatch.cs
@@ -1,4 +1,4 @@
-using System.Collections.Generic;
+using System.Collections;
 using GameNetcodeStuff;
 using HarmonyLib;
 using MoreCompany.Cosmetics;
@@ -8,56 +8,86 @@ namespace Unobscured.Patches
 {
     public class HideCosmeticsPatch
     {
-        private static bool isFirstPerson = false;
+        private static PlayerControllerB? lastSpectatedPlayer;
         private const float FIRST_PERSON_DISTANCE_THRESHOLD = 0.5f;
-/*        private static List<CosmeticInstance> cosmetics = new();
 
-        [HarmonyPostfix, HarmonyPatch(typeof(CosmeticApplication), nameof(CosmeticApplication.OnEnable))]
-        private static void OnEnablePatch(CosmeticApplication __instance)
+        [HarmonyPostfix, HarmonyPatch(typeof(PlayerControllerB), nameof(PlayerControllerB.SpectateNextPlayer))]
+        private static void SpectateNextPatch(PlayerControllerB __instance)
         {
-            StartOfRound.Instance.CameraSwitchEvent.AddListener(() =>
+            if (lastSpectatedPlayer != null)
             {
-                cosmetics = __instance.spawnedCosmetics;
-            });
-        }*/
+                var lastCosmeticApplication = getCosmeticApplicationFromPlayer(lastSpectatedPlayer);
+                if (lastCosmeticApplication == null) return;
+
+                Unobscured.Logger.LogDebug("Showing last player's cosmetics");
+
+                foreach (var cosmetic in lastCosmeticApplication.spawnedCosmetics)
+                {
+                    cosmetic.gameObject.SetActive(true);
+                }
+
+            }
+
+            lastSpectatedPlayer = __instance.spectatedPlayerScript;
+
+        }
 
         [HarmonyPostfix, HarmonyPatch(typeof(PlayerControllerB), nameof(PlayerControllerB.LateUpdate))]
         private static void LateUpdatePatch(PlayerControllerB __instance)
         {
-            if (!__instance.hasBegunSpectating) return;
-            if (__instance.spectateCameraPivot == null || __instance.spectatedPlayerScript == null) return;
+            if (!__instance.hasBegunSpectating || __instance.spectateCameraPivot == null || __instance.spectatedPlayerScript == null) return;
 
             float distance = Vector3.Distance(
                 StartOfRound.Instance.spectateCamera.transform.position,
                 __instance.spectatedPlayerScript.visorCamera.transform.position
             );
 
-            Transform model = __instance.spectatedPlayerScript.transform.Find("ScavengerModel");
-            if (model == null)
-            {
-                Unobscured.Logger.LogError("Model is null");
-                return;
-            }
+            var cosmeticApplication = getCosmeticApplicationFromPlayer(__instance.spectatedPlayerScript);
+            if (cosmeticApplication == null) return;
 
-            Transform metarig = model.Find("metarig");
-            if (metarig == null)
-            {
-                Unobscured.Logger.LogError("Metarig is null");
-                return;
-            }
-
-            var cosmeticApplication = metarig.GetComponent<CosmeticApplication>();
-            if (cosmeticApplication == null)
-            {
-                Unobscured.Logger.LogError("CosmeticApplication is null");
-                return;
-            }
             var cosmetics = cosmeticApplication.spawnedCosmetics;
 
             foreach (var cosmetic in cosmetics)
             {
                 cosmetic.gameObject.SetActive(distance > FIRST_PERSON_DISTANCE_THRESHOLD);
             }
+        }
+
+        [HarmonyPostfix, HarmonyPatch(typeof(StartOfRound), nameof(StartOfRound.ReviveDeadPlayers))]
+        private static void RevivePlayersPatch()
+        {
+            Unobscured.Logger.LogDebug("Force showing cosmetics");
+
+            if (lastSpectatedPlayer == null)
+            {
+                Unobscured.Logger.LogDebug("Last spectated player is null, not doing anything");
+                return;
+            }
+
+            var cosmeticApplication = getCosmeticApplicationFromPlayer(lastSpectatedPlayer);
+            if (cosmeticApplication == null)
+            {
+                Unobscured.Logger.LogDebug("CosmeticApplication is null");
+                return;
+            }
+
+            foreach (var cosmetic in cosmeticApplication.spawnedCosmetics)
+            {
+                cosmetic.gameObject.SetActive(true);
+            }
+
+            lastSpectatedPlayer = null;
+
+        }
+
+        private static CosmeticApplication? getCosmeticApplicationFromPlayer(PlayerControllerB player) {
+            Transform model = player.transform.Find("ScavengerModel");
+            if (model == null) return null;
+
+            Transform metarig = model.Find("metarig");
+            if (metarig == null) return null;
+
+            return metarig.GetComponent<CosmeticApplication>();
         }
     }
 }
